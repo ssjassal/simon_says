9f7ab7fe44153ddb49f721acc6aa2f6dc3861ce3
Soniya Jassal <jassal_soniya@bah.com>
2017-03-26 11:46:55 -0400

    Commiting for createBoard function (an inactive board) and UI with scoring

--- Project_1/js/app.js
+++ Project_1/js/app.js
@@ -1,6 +1,40 @@
 console.log('Simon has been initialized');
 
 $(function(){
-	
+	console.log('In the jquery');
 
+//====================================================
+//Grab Elements
+//====================================================
+	createBoard();
+//====================================================
+//Data & App Logic
+//====================================================
+
+//====================================================
+//Event Handlers
+//====================================================
+
+//====================================================
+//Event Listeners
+//====================================================	
+
+	console.log('outside FOR Loop');
 });
+
+//====================================================
+//DOM Manipulation Functions
+//====================================================
+
+var createBoard = function(){
+	for (var i = 0; i < 4; i++) {
+		// console.log('in FOR Loop: ' + i);
+		$content = $('#content');
+		var $square = $('<div>').addClass('square').attr('id','button' + i);
+		$square.css('opacity','.5');
+		// console.log($square);
+		$content.append($square);
+	}
+}
+
+
