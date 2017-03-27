66ad9fa7ff51a8afed2668d698457d5654d0d9e7
Soniya Jassal <jassal_soniya@bah.com>
2017-03-27 00:23:46 -0400

    Commiting for Easy Mode Level Array completed

--- Project_1/js/app.js
+++ Project_1/js/app.js
@@ -15,13 +15,18 @@ $(function(){
 //====================================================
 //Data & App Logic
 //====================================================
+	$levelSequence = [];
 
+	$round = 0;
+	$playerScore = 0;
+	$simonScore = 0;
 //====================================================
 //Event Handlers
 //====================================================
 	var determineLevel = function(){
 		
-		console.log($(this).attr('class'));
+		console.log('In the determineLevel function');
+		//console.log($(this).attr('class'));
 
 		if($(this).attr('class') == 'button')
 		{
@@ -42,7 +47,7 @@ $(function(){
 			$(this).addClass('clicked3');
 			// $levelButton.on('click', determineLevel);
 
-		} else if($(this).attr('class') == 'button clicked3')
+		}else if($(this).attr('class') == 'button clicked3')
 		{
 			$('#content p').text('Extreme');
 			$(this).removeClass('clicked3');
@@ -51,9 +56,95 @@ $(function(){
 		}else if($(this).attr('class') == "button clicked4")
 		{
 			$(this).removeClass('clicked4');
-		}
-			
+		}		
+	}
 
+	var makeLevel	= function()
+	{
+
+		console.log('In the makeLevel function');
+		$level = $('#content p').text();
+
+		console.log($level);
+
+		if($level == 'Easy')
+		{	
+			console.log($level);
+			for (var i = 1; i < 6; i++) 
+			{
+				$levelArray = [];
+				console.log('In the makeLevel FOR loop: ' + i);
+				if(i == 1)
+				{
+					console.log('round 1')
+					for (var j = 0; j < 5; j++) 
+					{
+						console.log(j);
+						$levelArray.push(Math.floor(Math.random()*4));
+						
+						console.log($levelArray);
+						
+					}
+					$levelSequence.push($levelArray);
+					console.log($levelSequence);
+				}else if(i == 2)
+				{
+					for (var j = 0; j < 6; j++) 
+					{
+						// $levelArray.push(Math.floor(Math.random()*4));
+						// $levelSequence['round'+ i] = $levelArray;
+						console.log(j);
+						$levelArray.push(Math.floor(Math.random()*4));
+						
+						console.log($levelArray);
+						// console.log($levelSequence);
+					}
+					$levelSequence.push($levelArray);
+				}else if(i == 3)
+				{
+					for (var j = 0; j < 7; j++) 
+					{
+						$levelArray.push(Math.floor(Math.random()*4));
+						// $levelSequence['round'+ i] = $levelArray;
+						console.log($levelArray);
+						// console.log($levelSequence);
+					}
+					$levelSequence.push($levelArray);
+				}else if(i == 4)
+				{
+					for (var j = 0; j < 8; j++) 
+					{
+						$levelArray.push(Math.floor(Math.random()*4));
+						// $levelSequence['round'+ i] = $levelArray;
+					}
+					$levelSequence.push($levelArray);
+				}else if(i == 5)
+				{
+					for (var j = 0; j < 9; j++) 
+					{
+						$levelArray.push(Math.floor(Math.random()*4));
+						// $levelSequence['round'+ i] = $levelArray;
+					}
+					$levelSequence.push($levelArray);
+				}
+				// else 
+				//    {
+				//    		return;
+				//    }
+			}
+			console.log($levelSequence);
+		}
+		else if($level == 'Medium')
+		{
+			console.log('not so bad');
+		}
+		else if($level == 'Hard')
+		{
+			console.log('oh em gee');
+		}else
+		{
+			console.log('uh-oh');
+		}
 
 	}
 //====================================================
@@ -61,6 +152,7 @@ $(function(){
 //====================================================	
 
 	$levelButton.on('click', determineLevel);
+	$startGame.on('click', makeLevel)
 
 });
 
@@ -72,9 +164,10 @@ var createBoard = function(){
 	for (var i = 0; i < 4; i++) {
 		// console.log('in FOR Loop: ' + i);
 		$content = $('#content');
-		var $square = $('<div>').addClass('square').attr('id','button' + i);
+		var $square = $('<div>').attr('class',i).attr('id','button' + i);
+		$square.addClass('square');
 		$square.css('opacity','.5');
-		// console.log($square);
+		//console.log($square);
 		$content.append($square);
 	}
 }
